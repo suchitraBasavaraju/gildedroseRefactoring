b1a086036dc5fbbce84c47de15e1045f3bb21671
Suchitra <suchitra.basavaraju@thoughtworks.com>
2022-12-15 00:24:44 +0530

    Refactor. update quailty based on products duplicated to all cases

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -12,11 +12,155 @@ func UpdateQuality(items []*Item) {
 
 }
 
-func updateItemQuality(item *Item) {
-	const AGEDBRIE = "Aged Brie"
-	const BACKSTAGEPASSES = "Backstage passes to a TAFKAL80ETC concert"
-	const SULFURAS = "Sulfuras, Hand of Ragnaros"
+const AGEDBRIE = "Aged Brie"
+const BACKSTAGEPASSES = "Backstage passes to a TAFKAL80ETC concert"
+const SULFURAS = "Sulfuras, Hand of Ragnaros"
 
+func updateItemQuality(item *Item) {
+	switch item.Name {
+	case AGEDBRIE:
+		updateAgedBrieQuality(item)
+		break
+	case BACKSTAGEPASSES:
+		updatebackStageQuality(item)
+		break
+	case SULFURAS:
+		updateSulfurasQuality(item)
+		break
+	default:
+		updateRegularProductQuality(item)
+
+	}
+
+}
+
+func updateRegularProductQuality(item *Item) {
+	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
+		if item.Quality < 50 {
+			qualityAdjustment(item, 1)
+			if item.Name == BACKSTAGEPASSES {
+				if item.SellIn < 11 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+				if item.SellIn < 6 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+			}
+		}
+	} else if item.Quality > 0 {
+		if item.Name != SULFURAS {
+			qualityAdjustment(item, -1)
+		}
+	}
+
+	if item.Name != SULFURAS {
+		item.SellIn = item.SellIn - 1
+	}
+
+	if item.SellIn < 0 {
+		if item.Name == AGEDBRIE {
+			if item.Quality < 50 {
+				qualityAdjustment(item, 1)
+			}
+		} else if item.Name == BACKSTAGEPASSES {
+			item.Quality = item.Quality - item.Quality
+		} else if item.Quality > 0 {
+			if item.Name != SULFURAS {
+				qualityAdjustment(item, -1)
+			}
+		}
+	}
+}
+
+func updateSulfurasQuality(item *Item) {
+	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
+		if item.Quality < 50 {
+			qualityAdjustment(item, 1)
+			if item.Name == BACKSTAGEPASSES {
+				if item.SellIn < 11 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+				if item.SellIn < 6 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+			}
+		}
+	} else if item.Quality > 0 {
+		if item.Name != SULFURAS {
+			qualityAdjustment(item, -1)
+		}
+	}
+
+	if item.Name != SULFURAS {
+		item.SellIn = item.SellIn - 1
+	}
+
+	if item.SellIn < 0 {
+		if item.Name == AGEDBRIE {
+			if item.Quality < 50 {
+				qualityAdjustment(item, 1)
+			}
+		} else if item.Name == BACKSTAGEPASSES {
+			item.Quality = item.Quality - item.Quality
+		} else if item.Quality > 0 {
+			if item.Name != SULFURAS {
+				qualityAdjustment(item, -1)
+			}
+		}
+	}
+}
+
+func updatebackStageQuality(item *Item) {
+	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
+		if item.Quality < 50 {
+			qualityAdjustment(item, 1)
+			if item.Name == BACKSTAGEPASSES {
+				if item.SellIn < 11 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+				if item.SellIn < 6 {
+					if item.Quality < 50 {
+						qualityAdjustment(item, 1)
+					}
+				}
+			}
+		}
+	} else if item.Quality > 0 {
+		if item.Name != SULFURAS {
+			qualityAdjustment(item, -1)
+		}
+	}
+
+	if item.Name != SULFURAS {
+		item.SellIn = item.SellIn - 1
+	}
+
+	if item.SellIn < 0 {
+		if item.Name == AGEDBRIE {
+			if item.Quality < 50 {
+				qualityAdjustment(item, 1)
+			}
+		} else if item.Name == BACKSTAGEPASSES {
+			item.Quality = item.Quality - item.Quality
+		} else if item.Quality > 0 {
+			if item.Name != SULFURAS {
+				qualityAdjustment(item, -1)
+			}
+		}
+	}
+}
+
+func updateAgedBrieQuality(item *Item) {
 	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
 		if item.Quality < 50 {
 			qualityAdjustment(item, 1)
