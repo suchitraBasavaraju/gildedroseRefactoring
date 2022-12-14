f53f6377af8b649a03e8b8f8bb3a27e344d350eb
Suchitra <suchitra.basavaraju@thoughtworks.com>
2022-12-15 00:30:32 +0530

    Refactor. update quality of back stage pass products

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -52,44 +52,22 @@ func updateSulfurasQuality(item *Item) {
 }
 
 func updatebackStageQuality(item *Item) {
-	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
-		if item.Quality < 50 {
-			qualityAdjustment(item, 1)
-			if item.Name == BACKSTAGEPASSES {
-				if item.SellIn < 11 {
-					if item.Quality < 50 {
-						qualityAdjustment(item, 1)
-					}
-				}
-				if item.SellIn < 6 {
-					if item.Quality < 50 {
-						qualityAdjustment(item, 1)
-					}
-				}
-			}
-		}
-	} else if item.Quality > 0 {
-		if item.Name != SULFURAS {
-			qualityAdjustment(item, -1)
-		}
-	}
-
-	if item.Name != SULFURAS {
-		item.SellIn = item.SellIn - 1
-	}
-
-	if item.SellIn < 0 {
-		if item.Name == AGEDBRIE {
+	if item.Quality < 50 {
+		qualityAdjustment(item, 1)
+		if item.SellIn < 11 {
 			if item.Quality < 50 {
 				qualityAdjustment(item, 1)
 			}
-		} else if item.Name == BACKSTAGEPASSES {
-			item.Quality = item.Quality - item.Quality
-		} else if item.Quality > 0 {
-			if item.Name != SULFURAS {
-				qualityAdjustment(item, -1)
+		}
+		if item.SellIn < 6 {
+			if item.Quality < 50 {
+				qualityAdjustment(item, 1)
 			}
 		}
+	}
+	item.SellIn = item.SellIn - 1
+	if item.SellIn < 0 {
+		item.Quality = 0
 	}
 }
 
