8159063e8864b5e50839b530a6a9de886191d016
Suchitra <suchitra.basavaraju@thoughtworks.com>
2022-12-15 00:28:02 +0530

    Refactor. update quality of sulfuras

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -48,45 +48,7 @@ func updateRegularProductQuality(item *Item) {
 }
 
 func updateSulfurasQuality(item *Item) {
-	if item.Name == AGEDBRIE || item.Name == BACKSTAGEPASSES {
-		if item.Quality < 50 {
-			qualityAdjustment(item, 1)
-			if item.Name == BACKSTAGEPASSES {
-				if item.SellIn < 11 {
-					if item.Quality < 50 {
-						qualityAdjustment(item, 1)
-					}
-				}
-				if item.SellIn < 6 {
-					if item.Quality < 50 {
-						qualityAdjustment(item, 1)
-					}
-				}
-			}
-		}
-	} else if item.Quality > 0 {
-		if item.Name != SULFURAS {
-			qualityAdjustment(item, -1)
-		}
-	}
-
-	if item.Name != SULFURAS {
-		item.SellIn = item.SellIn - 1
-	}
-
-	if item.SellIn < 0 {
-		if item.Name == AGEDBRIE {
-			if item.Quality < 50 {
-				qualityAdjustment(item, 1)
-			}
-		} else if item.Name == BACKSTAGEPASSES {
-			item.Quality = item.Quality - item.Quality
-		} else if item.Quality > 0 {
-			if item.Name != SULFURAS {
-				qualityAdjustment(item, -1)
-			}
-		}
-	}
+	//do nothing
 }
 
 func updatebackStageQuality(item *Item) {
