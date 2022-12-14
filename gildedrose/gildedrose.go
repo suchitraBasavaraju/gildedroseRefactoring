bb927c10c46143902e510487b6c57f6e06e6d87e
Suchitra <suchitra.basavaraju@thoughtworks.com>
2022-12-15 00:16:20 +0530

    Refactor. Extract string to named constants

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -13,16 +13,20 @@ func UpdateQuality(items []*Item) {
 }
 
 func updateItemQuality(item *Item) {
-	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+	const AGEDBRIE = "Aged Brie"
+	const BACKSTAGEPASSES = "Backstage passes to a TAFKAL80ETC concert"
+	const SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+	if item.Name != AGEDBRIE && item.Name != BACKSTAGEPASSES {
 		if item.Quality > 0 {
-			if item.Name != "Sulfuras, Hand of Ragnaros" {
+			if item.Name != SULFURAS {
 				item.Quality = item.Quality - 1
 			}
 		}
 	} else {
 		if item.Quality < 50 {
 			item.Quality = item.Quality + 1
-			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+			if item.Name == BACKSTAGEPASSES {
 				if item.SellIn < 11 {
 					if item.Quality < 50 {
 						item.Quality = item.Quality + 1
@@ -37,15 +41,15 @@ func updateItemQuality(item *Item) {
 		}
 	}
 
-	if item.Name != "Sulfuras, Hand of Ragnaros" {
+	if item.Name != SULFURAS {
 		item.SellIn = item.SellIn - 1
 	}
 
 	if item.SellIn < 0 {
-		if item.Name != "Aged Brie" {
-			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.Name != AGEDBRIE {
+			if item.Name != BACKSTAGEPASSES {
 				if item.Quality > 0 {
-					if item.Name != "Sulfuras, Hand of Ragnaros" {
+					if item.Name != SULFURAS {
 						item.Quality = item.Quality - 1
 					}
 				}
